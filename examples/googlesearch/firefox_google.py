aa3c96f9249c63a85d5313d3b597da5dc46701ca
Elephantatech <vivekm.mistry@gmail.com>
2019-06-06 13:22:38 -0400

    Update firefox_google.py
    created reusable function and closes firefox after it is done with the test

--- examples/googlesearch/firefox_google.py
+++ examples/googlesearch/firefox_google.py
@@ -4,6 +4,7 @@ using firefox browser.
 """
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from time import sleep
 
 def search_apples():
     """This function will open google.com on firefox,
@@ -15,6 +16,21 @@ def search_apples():
     query_element = driver.find_element_by_name('q')
     query_element.send_keys("Apples")
     query_element.submit()
+    sleep(10)
+    driver.close()
+
+def search_google(search_term):
+    """This function will open google.com on firefox,
+    then search for anything on google"""
+    with webdriver.Firefox() as driver:
+        driver.get("https://www.google.com")
+        assert "Google" in driver.title 
+        # if the window loaded title is Google then continue below
+        query_element = driver.find_element_by_name('q')
+        query_element.send_keys(search_term)
+        query_element.submit()
+        sleep(10)
+        
 
 if __name__ == "__main__":
-    search_apples()
+    search_google("Pineapples")
